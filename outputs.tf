98886544f96448aa6d99954c600ba908627ffb6e
stafaniasaju <stafania.saju@ibm.com>
2024-06-25 15:32:49 +0200

    outputs

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "console_log" {
   description = "The console logs from python script execution."
   value       = data.local_file.console_log_file.content
 }
-/*
+
 output "pi_image_operation_results" {
   description = "The log object that contains all the accounts and their workspaces for which the image import/delete requests were success, skipped and failed."
   value       = local.pi_image_operation_results
@@ -12,4 +12,4 @@ output "pi_image_status_results" {
   description = "The log object that contains all the accounts and their workspaces for which the provided image is active and inactive."
   value       = local.pi_image_status_results
 }
-*/
+
